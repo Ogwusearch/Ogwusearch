1ffa82197c30a375142854fef73a8bad1c073cf4
Ogwusearch <icebergi19@proton.me>
2026-09-24 05:08:33 -0700

    feat: establish engineering foundation

---
+++ packages/solar-engine/src/load/calculation/index.ts
@@ -0,0 +1,12 @@
+export {
+  calculateLoad,
+} from "./calculate-load.js";
+
+export {
+  calculateDailyEnergy,
+  calculateMonthlyEnergy,
+} from "./calculate-energy.js";
+
+export {
+  calculatePeakDemand,
+} from "./calculate-peak-demand.js";
